fba559a4128e88934d023be5466389c6c7c07f6c
Sam Tebbs <samuel.tebbs@gmail.com>
2017-01-16 20:33:19 +0000

    Added player progress to Player

--- src/game/core/player/Player.java
+++ src/game/core/player/Player.java
@@ -7,6 +7,7 @@ public class Player {
 
     public final String name;
     public final PlayerStatus status = new PlayerStatus();
+    private double progress = 0;
 
     public Player(String name) {
         this.name = name;
@@ -15,5 +16,30 @@ public class Player {
     public void update() {
         status.update(this);
     }
-    
+
+    public void setProgress(double progress) {
+        this.progress += progress;
+        if(this.progress >= 100) {
+            onProgressDone();
+            this.progress = 0;
+        }
+    }
+
+    private void onProgressDone() {
+        // TODO
+    }
+
+    public void removeProgress() {
+
+    }
+
+    public void addProgress() {
+        double toAdd = 1.0 * getProgressMultiplier();
+        setProgress(progress + toAdd);
+    }
+
+    private double getProgressMultiplier() {
+        return status.getAttribute(PlayerStatus.PlayerAttribute.PRODUCTIVITY);
+    }
+
 }
