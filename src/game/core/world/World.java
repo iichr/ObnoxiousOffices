47f3fa145321a8f8586c80cad16cc52121c9dc28
Sam Tebbs <samuel.tebbs@gmail.com>
2017-01-20 17:40:13 +0000

    Added World.load and made World.checkBounds public

--- src/game/core/world/World.java
+++ src/game/core/world/World.java
@@ -3,9 +3,13 @@ package game.core.world;
 import game.core.Updateable;
 import game.core.player.Player;
 import game.core.world.tile.Tile;
+import game.core.world.tile.TileType;
 
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 /**
  * Created by samtebbs on 19/01/2017.
@@ -45,8 +49,23 @@ public class World implements Updateable {
         if(checkBounds(x, y, z)) tiles[x][y][z] = tile;
     }
 
-    private boolean checkBounds(int x, int y, int z) {
+    public boolean checkBounds(int x, int y, int z) {
         return x < tiles.length && x >= 0 && y < tiles[x].length && y >= 0 && z < tiles[x][y].length && z >= 0;
+    }
+
+    public static World load(String[] lines, int maxPlayers) {
+        int sizeX = Arrays.stream(lines).mapToInt(String::length).min().orElse(0), sizeY = lines.length, sizeZ = 1;
+        World world = new World(maxPlayers, sizeX, sizeY, sizeZ);
+        IntStream.range(0, sizeY).forEach(y -> {
+            IntStream.range(0, sizeX).forEach(x -> {
+                IntStream.range(0, sizeZ).forEach(z -> {
+                    char ch = lines[y].charAt(x);
+                    Location location = new Location(x, y, z, world);
+                    if(TileType.typeExists(ch)) location.setTile(new Tile(location, TileType.getType(ch)));
+                });
+            });
+        });
+        return world;
     }
 
 }
