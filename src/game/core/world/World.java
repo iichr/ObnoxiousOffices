7ea7e167694ba40c33d313cfa1e54c606afd6cd9
Sam Tebbs <samuel.tebbs@gmail.com>
2017-01-23 10:06:29 +0000

    Added World.load(Path, ...)

--- src/game/core/world/World.java
+++ src/game/core/world/World.java
@@ -5,9 +5,13 @@ import game.core.player.Player;
 import game.core.world.tile.Tile;
 import game.core.world.tile.TileType;
 
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.IntStream;
 
@@ -19,11 +23,15 @@ public class World implements Updateable {
     private final Set<Player> players;
     private final int maxPlayers;
     private final Tile[][][] tiles;
+    public final int xSize, ySize, zSize;
 
     public World(int maxPlayers, int sizeX, int sizeY, int sizeZ) {
         this.tiles = new Tile[sizeX][sizeY][sizeZ];
         this.players = new HashSet<>(maxPlayers);
         this.maxPlayers = maxPlayers;
+        zSize = sizeZ;
+        ySize = sizeY;
+        xSize = sizeX;
     }
 
     public void addPlayer(Player player) {
@@ -66,6 +74,11 @@ public class World implements Updateable {
             });
         });
         return world;
+    }
+
+    public static World load(Path path, int maxPlayers) throws IOException {
+        List<String> l = Files.readAllLines(path);
+        return load(l.toArray(new String[l.size()]), maxPlayers);
     }
 
 }
