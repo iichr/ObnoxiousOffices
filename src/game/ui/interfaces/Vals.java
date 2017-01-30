5c48c20736038a4e61027f5cea15d4ca7d1ff873
ywc572 <ywc572@cs.bham.ac.uk>
2017-01-30 15:15:22 +0000

    Changed character and play state ID , added value for inputs

--- src/game/ui/interfaces/Vals.java
+++ src/game/ui/interfaces/Vals.java
@@ -1,5 +1,7 @@
 package game.ui.interfaces;
 
+import org.newdawn.slick.Input;
+
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
@@ -21,10 +23,10 @@ public interface Vals {
 	// STATE IDs
 	public static final int EXIT = -1;	
 	public static final int MENU_STATE = 0;
-	public static final int PLAY_STATE = 1;
+    public static final int CHARACTER_SELECT_STATE = 1;
 	public static final int OPTIONS_STATE = 2;
 	public static final int RULES_STATE = 3;
-	public static final int CHARACTER_SELECT_STATE = 4;
+    public static final int PLAY_STATE = 4;
 
 	// BUTTON SIZES
 	public static final int BUTTON_WIDTH = 200;
@@ -33,4 +35,12 @@ public interface Vals {
 	// The coords of the screen centre for a Menu Button
 	public static final int BUTTON_ALIGN_CENTRE_W = SCREEN_WIDTH / 2 - BUTTON_WIDTH / 2;
 	public static final int BUTTON_ALIGN_CENTRE_H = SCREEN_HEIGHT / 2 - BUTTON_HEIGHT / 2;
+    // Input checking
+    public static final long INPUT_INTERVAL = 100;
+    // Get input
+    public static final int UP = Input.KEY_UP;
+    public static final int DOWN = Input.KEY_DOWN;
+    public static final int LEFT = Input.KEY_LEFT;
+    public static final int RIGHT = Input.KEY_RIGHT;
+    public static final int ENTER = Input.KEY_ENTER;
 }
