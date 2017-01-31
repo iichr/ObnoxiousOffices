5f7938cc0882687a6136380df088b0d3b8e1a12a
Sam Tebbs <samuel.tebbs@gmail.com>
2017-01-31 11:29:27 +0000

    Added JavaDocs to PlayerAction

--- src/game/core/player/action/PlayerAction.java
+++ src/game/core/player/action/PlayerAction.java
@@ -16,12 +16,26 @@ public abstract class PlayerAction implements Updateable {
 
     public abstract void update();
 
+    /**
+     * Start the action
+     */
     public abstract void start();
 
+    /**
+     * Cancel the action
+     */
     public abstract void cancel();
 
+    /**
+     * Returns true if the action can be canceled
+     * @return (see above)
+     */
     public abstract boolean cancelable();
 
+    /**
+     * Returns true if the action has ended
+     * @return (see above)
+     */
     public abstract boolean ended();
 
 }
