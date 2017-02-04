99e00471df684b2689605a3b3200ebeea32e5b6b
Chris Iliev <cii549@cs.bham.ac.uk>
2017-02-04 12:50:14 +0000

    Added served address text field to Character selections screen.

--- src/game/ui/states/CharacterSelect.java
+++ src/game/ui/states/CharacterSelect.java
@@ -1,9 +1,17 @@
 package game.ui.states;
+import java.awt.Font;
+
 import org.lwjgl.input.Mouse;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.UnicodeFont;
+import org.newdawn.slick.font.effects.ColorEffect;
+import org.newdawn.slick.gui.AbstractComponent;
+import org.newdawn.slick.gui.ComponentListener;
+import org.newdawn.slick.gui.TextField;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -12,15 +20,20 @@ import game.ui.buttons.SelectionButton;
 import game.ui.interfaces.ImageLocations;
 import game.ui.interfaces.Vals;
 
+
 public class CharacterSelect extends BasicGameState {
 	private MenuButton connectButton, backButton;
 	private SelectionButton circleButton;
 	private String mouseCoords = "No input yet!";
-
+	
+	private TextField serverAddress;
+	private UnicodeFont font1;
+		
 	public CharacterSelect(int state) {
 
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void init(GameContainer gc, StateBasedGame game) throws SlickException {
 		Image back = new Image(ImageLocations.BACK);
@@ -36,17 +49,34 @@ public class CharacterSelect extends BasicGameState {
 		Image circleSelected = new Image(ImageLocations.CIRCLE_SELECTED, false, Image.FILTER_NEAREST);
 		circleButton = new SelectionButton(Vals.BUTTON_ALIGN_CENTRE_W - 200, Vals.BUTTON_ALIGN_CENTRE_H - 200, 50, 50, circleUnselected, circleSelected);
 		
+		// Server address text field.
+		font1 = new UnicodeFont(new Font("Arial", Font.BOLD, 20));
+		font1.addAsciiGlyphs();
+		// necessary to load an effect otherwise an exception is thrown.
+		font1.getEffects().add(new ColorEffect());
+		font1.loadGlyphs();
+		
+		serverAddress = new TextField(gc,font1, (Vals.SCREEN_WIDTH - 300)/2, 200, 300, font1.getLineHeight(), new ComponentListener(){
+			public void componentActivated(AbstractComponent src){	
+				serverAddress.setFocus(true);
+			}
+		});
+		serverAddress.setBackgroundColor(Color.white);
+		serverAddress.setTextColor(Color.black);
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame game, Graphics g) throws SlickException {
 		// debugging
 		g.drawString(mouseCoords, 10, 50);
+		//g.drawString(ipAddress.getText(), 700, 100);
 
 		// add back button
 		backButton.render(g);
 		circleButton.render(g);
 		connectButton.render(g);
+		
+		serverAddress.render(gc, g);
 	}
 
 	@Override
