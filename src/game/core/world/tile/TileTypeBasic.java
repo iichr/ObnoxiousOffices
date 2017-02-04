c7b0bb89d427409a11628825b5f50bf7886a487d
Sam Tebbs <samuel.tebbs@gmail.com>
2017-02-04 12:39:12 +0000

    Made TileTypeBasic not override canWalkOver

--- src/game/core/world/tile/TileTypeBasic.java
+++ src/game/core/world/tile/TileTypeBasic.java
@@ -5,16 +5,11 @@ import game.core.player.Player;
 /**
  * Created by samtebbs on 20/01/2017.
  */
-public class TileTypeBasic extends TileType {
+public abstract class TileTypeBasic extends TileType {
 
     @Override
     public void onWalkOver(Player player) {
 
-    }
-
-    @Override
-    public boolean canWalkOver() {
-        return true;
     }
 
     @Override
