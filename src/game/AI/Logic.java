2e49090fb1954cd1a1c7c69c00daaac87e475d8f
Atanas Harbaliev <atanas.harbaliev96@gmail.com>
2017-02-06 19:40:09 +0000

    Minor changes

--- src/game/AI/Logic.java
+++ src/game/AI/Logic.java
@@ -4,20 +4,75 @@ import game.core.player.Player;
 import game.core.world.World;
 
 public interface Logic {
-	
+
+	/**
+	 * Tells the bot/bots what to do while the player is drinking coffee.
+	 */
 	public void reactToPlayerDrink();
-	
+
+	/**
+	 * Tells the bot/bots what to do while the player is working on his own
+	 * project.
+	 */
 	public void reactToPlayerWork();
-	
+
+	/**
+	 * Tells the bot/bots what to do while the player is trying to hack
+	 * its/their computer/computers.
+	 */
 	public void reactToPlayerHack();
-	
-	public void gainEnergy();
-	
+
+	/**
+	 * Finds the shortest path to the coffee machine from the current place
+	 * 
+	 * @param w
+	 *            the world
+	 * @param p
+	 *            the player that needs to go to the coffee machine
+	 * @param cordI
+	 *            the i coordinate of the coffee machine
+	 * @param cordJ
+	 *            the j coordinate of the coffee machine
+	 */
 	public void findCoffeeMachine(World w, Player p, int i, int j);
-	
+
+	/**
+	 * Finds the shortest path to the bed from the current place
+	 * 
+	 * @param w
+	 *            the world
+	 * @param p
+	 *            the player that needs to go to the coffee machine
+	 * @param cordI
+	 *            the i coordinate of the bed
+	 * @param cordJ
+	 *            the j coordinate of the bed
+	 */
 	public void findBed(World w, Player p, int i, int j);
-	
-	public void goToCoffeeMachine();
-	
-	public void goToBed();
+
+	/**
+	 * If energy is below a certain level, refresh. Will change chance of
+	 * getting hyperactive from coffe for different difficulties.
+	 * 
+	 * @param p
+	 *            the player that needs to go
+	 * @param i
+	 *            i coordinate of the closest coffee machine on the map
+	 * @param j
+	 *            j coordinate of the closest coffee machine on the map
+	 */
+	public void goToCoffeeMachine(Player p, int i, int j);
+
+	/**
+	 * If energy is below a certain level, refresh. Sleeping on the sofa takes
+	 * longer than drinking cofee but doesn't have side effects
+	 * 
+	 * @param p
+	 *            the player that needs to sleep
+	 * @param i
+	 *            i coordinate of the closest sofa on the map
+	 * @param j
+	 *            j coordinate of the closest sofa on the map
+	 */
+	public void goToBed(Player p, int i, int j);
 }
--- src/game/AI/LogicEasy.java
+++ src/game/AI/LogicEasy.java
@@ -3,6 +3,8 @@ package game.AI;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import javax.xml.ws.handler.LogicalHandler;
+
 import game.core.player.Player;
 import game.core.world.World;
 
@@ -10,7 +12,7 @@ import game.core.world.World;
  * @author Atanas Harbaliev Created on 18/01/2017
  */
 
-public class LogicEasy implements Logic{
+public class LogicEasy implements Logic {
 
 	// thresholds for attributes
 	public final double energyThreshold = 0.2;
@@ -21,53 +23,28 @@ public class LogicEasy implements Logic{
 	//paths
 	public ArrayList<Integer> toBed, toCM, fromBed, fromCM;
 
-	/**
-	 * Tells the bot/bots what to do while the player is drinking coffee.
-	 */
+
 	@Override
 	public void reactToPlayerDrink() {
 		// TODO
 	}
 
-	/**
-	 * Tells the bot/bots what to do while the player is working on his own
-	 * project.
-	 */
+
 	@Override
 	public void reactToPlayerWork() {
 		// TODO
 	}
 
-	/**
-	 * Tells the bot/bots what to do while the player is trying to hack
-	 * its/their computer/computers.
-	 */
 	@Override
 	public void reactToPlayerHack() {
 		// TODO
 	}
 
-	/**
-	 * If energy is below a certain level, refresh. Will change chance of
-	 * getting hyperactive from coffe for different difficulties.
-	 */
 	@Override
-	public void gainEnergy() {
+	public void goToCoffeeMachine() {
 
 	}
 
-	/**
-	 * Finds the shortest path to the coffee machine from the current place
-	 * 
-	 * @param w
-	 *            the world
-	 * @param p
-	 *            the player that needs to go to the coffee machine
-	 * @param cordI
-	 *            the i coordinate of the coffee machine
-	 * @param cordJ
-	 *            the j coordinate of the coffee machine
-	 */
 	@Override
 	public void findCoffeeMachine(World w, Player p, int cordI, int cordJ) {
 		// create the list of blocks in the grid that represents the path
@@ -86,18 +63,6 @@ public class LogicEasy implements Logic{
 		toBed = path;
 	}
 
-	/**
-	 * Finds the shortest path to the bed from the current place
-	 * 
-	 * @param w
-	 *            the world
-	 * @param p
-	 *            the player that needs to go to the coffee machine
-	 * @param cordI
-	 *            the i coordinate of the bed
-	 * @param cordJ
-	 *            the j coordinate of the bed
-	 */
 	@Override
 	public void findBed(World w, Player p, int cordI, int cordJ) {
 		// create the list of blocks in the grid that represents the path
@@ -117,15 +82,4 @@ public class LogicEasy implements Logic{
 
 	}
 
-	@Override
-	public void goToCoffeeMachine() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void goToBed() {
-		// TODO Auto-generated method stub
-		
-	}
 }
