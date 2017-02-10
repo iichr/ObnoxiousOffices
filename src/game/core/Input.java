5820645276b80b335a85689da3e9a2d680c96cca
Sam Tebbs <samuel.tebbs@gmail.com>
2017-02-10 15:49:20 +0000

    Added PlayerInputEvent and Input

---
+++ src/game/core/Input.java
@@ -0,0 +1,12 @@
+package game.core;
+
+/**
+ * Created by samtebbs on 10/02/2017.
+ */
+public class Input {
+
+    public enum InputType {
+        MOVE_UP, INTERACT, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT
+    }
+
+}
