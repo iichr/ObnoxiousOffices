c14f6b248d1763084abd6e7b7270515fecbd8191
Sam Tebbs <samuel.tebbs@gmail.com>
2017-02-11 20:51:44 +0000

    Made PlayerMovedEvent extend PlayerEvent

--- src/game/core/event/PlayerMovedEvent.java
+++ src/game/core/event/PlayerMovedEvent.java
@@ -3,15 +3,14 @@ package game.core.event;
 /**
  * Created by samtebbs on 10/02/2017.
  */
-public class PlayerMovedEvent extends Event {
+public class PlayerMovedEvent extends PlayerEvent {
 
     public final int dX, dY, dZ;
-    public final String playerName;
 
     public PlayerMovedEvent(int dX, int dY, int dZ, String playerName) {
+        super(playerName);
         this.dX = dX;
         this.dY = dY;
         this.dZ = dZ;
-        this.playerName = playerName;
     }
 }
