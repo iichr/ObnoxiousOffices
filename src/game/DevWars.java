18031e7679c91a911f6e3b46f61d361a9280f5f3
axg539 <axg539@bham.ac.uk>
2017-02-12 13:12:30 +0000

    fixed DevWars class

--- src/game/DevWars.java
+++ src/game/DevWars.java
@@ -1,5 +1,6 @@
 package game;
 
+import game.core.ifc.Net;
 import game.networking.Client;
 import game.ui.Game;
 
@@ -8,7 +9,9 @@ public class DevWars {
 	public static Client client;
 
 	public static void main(String[] args) {
+		Net.initClient();
 		client = new Client();
 		Game g = new Game("DevWars");
+		g.init();
 	}
 }
