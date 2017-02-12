d3480a19fc5d4ae500b2e95f2199de349d1b630d
axg539 <axg539@bham.ac.uk>
2017-02-12 12:10:25 +0000

    add DevWars class

---
+++ src/game/DevWars.java
@@ -0,0 +1,10 @@
+import game.networking.Client;
+import game.ui.Game;
+
+public class DevWars {
+	
+	public static void main(String[] args) {
+		Game g = new Game("DevWars");
+		Client c = new Client();
+	}
+}
