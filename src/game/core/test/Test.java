5eb584941bc33dfa58e69f2e416fc529c7d17e4b
Aidan Goodwin <Aidan Goodwin>
2017-02-13 00:24:26 +0000

    re-added resting

---
+++ src/game/core/test/Test.java
@@ -0,0 +1,48 @@
+package game.core.test;
+
+import game.core.event.Event;
+import game.core.event.Events;
+import game.core.event.PlayerInputEvent;
+import game.core.player.Player;
+import game.core.world.Direction;
+import game.core.world.World;
+
+/**
+ * Created by samtebbs on 26/01/2017.
+ */
+public class Test {
+
+    public static String localPlayer;
+
+    static {
+        Test test = new Test();
+        Events.on(PlayerInputEvent.class, test::playerInput);
+    }
+
+    private void playerInput(PlayerInputEvent t) {
+        switch (t.inputType) {
+            case MOVE_DOWN:
+                getPlayer(localPlayer).setFacing(Direction.SOUTH);
+                break;
+            case MOVE_UP:
+            	getPlayer(localPlayer).setFacing(Direction.NORTH);
+                break;
+            case MOVE_LEFT:
+            	getPlayer(localPlayer).setFacing(Direction.WEST);
+                break;
+            case MOVE_RIGHT:
+            	getPlayer(localPlayer).setFacing(Direction.EAST);
+                break;
+        }
+        getPlayer(localPlayer).moveForwards();
+    }
+    
+    private static Player getPlayer(String playerName) {
+        return getWorld().getPlayer(playerName);
+    }
+    
+    private static World getWorld() {
+        return World.world;
+    }
+
+}
