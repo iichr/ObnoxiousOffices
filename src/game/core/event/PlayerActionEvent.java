decf6569411d7c5f445e87168beba84bf71de3a6
Sam Tebbs <samuel.tebbs@gmail.com>
2017-02-16 10:30:29 +0000

    Made PlayerActionEvent, PlayerEvent and TimedPlayerAction implement Serializable

--- src/game/core/event/PlayerActionEvent.java
+++ src/game/core/event/PlayerActionEvent.java
@@ -2,10 +2,12 @@ package game.core.event;
 
 import game.core.player.action.PlayerAction;
 
+import java.io.Serializable;
+
 /**
  * Created by samtebbs on 11/02/2017.
  */
-public class PlayerActionEvent extends PlayerEvent {
+public class PlayerActionEvent extends PlayerEvent implements Serializable {
 
     public final PlayerAction action;
 
