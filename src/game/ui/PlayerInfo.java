682c21c482ea378583dfd22a5b0a4f09e9f1aaeb
axg539 <axg539@bham.ac.uk>
2017-02-17 13:19:11 +0000

    fixed tile display

--- src/game/ui/PlayerInfo.java
+++ src/game/ui/PlayerInfo.java
@@ -37,19 +37,17 @@ public class PlayerInfo {
 			g.drawString(p.name, (playerX - offsetX), (playerY - offsetY));
 			
 			if (p.name.equals(Player.localPlayerName)) {
-				System.out.println("in if statement");
 				Location inFront = pLocation.forward(p.getFacing());
 				Tile t = inFront.getTile();
-				System.out.println("tile type: " + t.type);
 
 				g.setColor(Color.black);
-				if (t.type == TileType.COMPUTER) {
+				if (t.type.equals(TileType.COMPUTER)) {
 					System.out.println("show pc");
 					g.drawString("WORK", (inFront.x * tileWidth), (inFront.y + 1) * (tileHeight / 2));
-				} else if (t.type == TileType.COFFEE_MACHINE) {
+				} else if (t.type.equals(TileType.COFFEE_MACHINE)) {
 					System.out.println("show cm");
 					g.drawString("DRINK", (inFront.x * tileWidth), (inFront.y + 1) * (tileHeight / 2));
-				} else if (t.type == TileType.SOFA) {
+				} else if (t.type.equals(TileType.SOFA)) {
 					System.out.println("show sofa");
 					g.drawString("NAP", (inFront.x * tileWidth), (inFront.y + 1) * (tileHeight / 2));
 				}
