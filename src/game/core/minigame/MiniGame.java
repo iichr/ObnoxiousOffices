a94f48b896bb937de1de6f8876ce7db9422e1ab7
Sam Tebbs <samuel.tebbs@gmail.com>
2017-02-18 13:23:44 +0000

    Added MiniGame

---
+++ src/game/core/minigame/MiniGame.java
@@ -0,0 +1,61 @@
+package game.core.minigame;
+
+import game.core.Updateable;
+import game.core.event.Events;
+import game.core.event.MiniGameEndedEvent;
+import game.core.event.PlayerInputEvent;
+import game.core.player.Player;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Created by samtebbs on 18/02/2017.
+ */
+public abstract class MiniGame implements Updateable {
+
+    public static final String SCORE = "SCORE";
+    protected boolean ended = false;
+    public final int MAX_SCORE = 2;
+
+    private Map<String, Map<String, Integer>> stats = new HashMap<>();
+    private List<String> players = new ArrayList<>();
+
+    public abstract void onInput(PlayerInputEvent event);
+
+    protected void setStat(String player, String stat, int val) {
+        if(!stats.containsKey(player)) {
+            addPlayer(player);
+        }
+        stats.get(player).put(stat, val);
+    }
+
+    protected void addPlayer(String player) {
+        stats.put(player, new HashMap<>());
+        players.add(player);
+    }
+
+    public int getStat(String player, String stat) {
+        if(!stats.containsKey(player) || !stats.get(player).containsKey(stat)) return 0;
+        return stats.get(player).get(stat);
+    }
+
+    public List<String> getPlayers() {
+        return players;
+    }
+
+    @Override
+    public void update() {
+        for (String player : getPlayers()) if (getStat(player, SCORE) == MAX_SCORE) end(player);
+    }
+
+    @Override
+    public boolean ended() {
+        return ended;
+    }
+
+    protected void end(String player) {
+        ended = true;
+        Events.trigger(new MiniGameEndedEvent(getPlayers(), player));
+    }
+}
