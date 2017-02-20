ef736a57ca9b1350b07662e40238c568ba9dbcce
Chris Iliev <cii549@cs.bham.ac.uk>
2017-02-20 23:44:11 +0000

    Hangman - implemented word methods + javadoc.

--- src/game/core/minigame/MiniGameHangman.java
+++ src/game/core/minigame/MiniGameHangman.java
@@ -1,19 +1,24 @@
 package game.core.minigame;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * @author iichr
  *
  */
 public class MiniGameHangman {
-	
-	private String[] dict = {"replace", "with", "dictionary", "ofatleast" , "100words"};
+
+	private String[] dict = { "replace", "with", "dictionary", "ofatleast", "100words" };
+	String word;
 	private int attempts = 0;
-	private int PERMITED_ATTEMPTS;
+	private int PERMITTED_ATTEMPTS;
 	private boolean guessed = false;
 	private char[] alreadyEntered;
 
 	public MiniGameHangman() {
-		// generate a word at random from list
+		word = pickWord(dict);
+		alreadyEntered = new char[word.length()];
 		setDifficulty();
 	}
 
@@ -22,11 +27,37 @@ public class MiniGameHangman {
 		// set PERMITTED_ATTEMPTS
 	}
 
+	/**
+	 * Check whether a letter has already been input by the user
+	 * 
+	 * @param c
+	 *            The letter to check for
+	 * @param entered
+	 *            The array of letters entered so far
+	 * @return
+	 */
 	private boolean checkAlreadyEntered(char c, char[] entered) {
-
+		return Arrays.asList(entered).indexOf(c) >= 0;
 	}
 
-	private boolean displayWord(String word, char[] entered) {
+	/**
+	 * Display a word.
+	 * 
+	 * @param word
+	 *            The word to display
+	 * @param entered
+	 *            The array of letters entered so far
+	 */
+	private void displayWord(String word, char[] entered) {
+		for (int i = 0; i < word.length(); i++) {
+			char letter = word.charAt(i);
+			if (checkAlreadyEntered(letter, entered))
+				// debugging
+				// if encountered before, display
+				System.out.print(letter);
+			else
+				System.out.print('_');
+		}
 
 	}
 
@@ -35,14 +66,14 @@ public class MiniGameHangman {
 	}
 
 	private boolean lost() {
-		if(attempts >= PERMITTED_ATTEMPTS) {
+		if (attempts >= PERMITTED_ATTEMPTS) {
 			return true;
 		}
 		return false;
 	}
 
 	private boolean won() {
-
+		return false;
 	}
 
 	private int getAttempts() {
@@ -51,5 +82,19 @@ public class MiniGameHangman {
 
 	private char[] getAlreadyEntered() {
 		return alreadyEntered;
+	}
+
+	/**
+	 * Pick a random word given an array of strings
+	 * 
+	 * @param dict
+	 * @return A random word.
+	 */
+	private String pickWord(String[] dict) {
+		Random randGenerator = new Random();
+		int i = randGenerator.nextInt(dict.length);
+		word = dict[i];
+
+		return word;
 	}
 }
