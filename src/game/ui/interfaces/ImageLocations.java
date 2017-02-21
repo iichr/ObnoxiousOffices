c0b3b26fedc01ccf57df4dffd3b46c8d9431748c
Aidan Goodwin <Aidan Goodwin>
2017-02-21 19:05:06 +0000

    adding dialogue boxes

--- src/game/ui/interfaces/ImageLocations.java
+++ src/game/ui/interfaces/ImageLocations.java
@@ -1,42 +1,47 @@
 package game.ui.interfaces;
 
 /**
- * An interface for all images in the UI. * 
+ * An interface for all images in the UI. *
  */
-public interface ImageLocations{
-	
-	//BUTTON IMAGES
+public interface ImageLocations {
+
+	// BUTTON IMAGES
 	public static final String PLAY = "res/buttons/play.png";
 	public static final String PLAY_ROLLOVER = "res/buttons/playR.png";
-	
+
 	public static final String OPTIONS = "res/buttons/options.png";
 	public static final String OPTIONS_ROLLOVER = "res/buttons/optionsR.png";
-	
+
 	public static final String RULES = "res/buttons/rules.png";
 	public static final String RULES_ROLLOVER = "res/buttons/rulesR.png";
-	
+
 	public static final String EXIT = "res/buttons/exit.png";
 	public static final String EXIT_ROLLOVER = "res/buttons/exitR.png";
-	
+
 	public static final String CONNECT = "res/buttons/connect.png";
 	public static final String CONNECT_ROLLOVER = "res/buttons/connectR.png";
-	
+
 	public static final String BACK = "res/buttons/back.png";
 	public static final String BACK_ROLLOVER = "res/buttons/backR.png";
-	
+
 	public static final String SPEAKER_ON = "res/buttons/speakerOn.png";
 	public static final String SPEAKER_OFF = "res/buttons/speakerOff.png";
-	
-	//SELECTION BUTTON IMAGES
-	public static final String CIRCLE_UNSELECTED = "res/buttons/circleUnselected.png";
-	public static final String CIRCLE_SELECTED = "res/buttons/circleSelected.png";
+
+	// SELECTION BUTTON IMAGES
 	public static final String BG = "res/bg.png";
-	
+
 	// PLAYER AVATAR IMAGES
 	public static final String TEMP_AVATAR = "res/player/avatar.png";
 
-	//STATUS IMAGES
+	//IN GAME UI ELEMENTS
+	public static final String PROGRESS_BAR_BASE = "/res/ui/progressBarBase.png";
+	public static final String PROGRESS_BAR_FULL = "/res/ui/progressBarFull.png";
+	public static final String WORK_DIALOGUE = "/res/ui/workDiag.png";
+	public static final String DRINK_DIALOGUE = "/res/ui/drinkDiag.png";
+	public static final String SLEEP_DIALOGUE = "/res/ui/sleepDiag.png";
+
+	// STATUS IMAGES
 	public static final String COFFEE = "";
 	public static final String COFFEE_END = "";
-	
+
 }
