5adb5f363a87b5d68d93b88fb5deab2dcddd52e4
Sam Tebbs <samuel.tebbs@gmail.com>
2017-02-22 11:39:24 +0000

    Made MiniGame store stats and vars as Objects

--- src/game/core/minigame/MiniGame.java
+++ src/game/core/minigame/MiniGame.java
@@ -2,10 +2,8 @@ package game.core.minigame;
 
 import game.core.Updateable;
 import game.core.event.*;
-import game.core.player.Player;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * Created by samtebbs on 18/02/2017.
@@ -18,20 +16,20 @@ public abstract class MiniGame implements Updateable {
 
     public static MiniGame localMiniGame;
 
-    private Map<String, Map<String, Integer>> stats = new HashMap<>();
-    private Map<String, Integer> vars = new HashMap<>();
+    private Map<String, Map<String, Object>> stats = new HashMap<>();
+    private Map<String, Object> vars = new HashMap<>();
     private List<String> players = new ArrayList<>();
 
     protected void addVar(String var, int val) {
-        setVar(var, getVar(var) + val);
+        setVar(var, getIntVar(var) + val);
     }
 
     protected void addStat(String player, String stat, int val) {
-        setStat(player, stat, getStat(player, stat) + val);
+        setStat(player, stat, getIntStat(player, stat) + val);
     }
 
     protected void negVar(String var) {
-        setVar(var, -getVar(var));
+        setVar(var, -getIntVar(var));
     }
 
     public abstract void onInput(PlayerInputEvent event);
@@ -54,13 +52,13 @@ public abstract class MiniGame implements Updateable {
         players.add(player);
     }
 
-    public int getStat(String player, String stat) {
-        if(!stats.containsKey(player) || !stats.get(player).containsKey(stat)) return 0;
+    public Object getStat(String player, String stat) {
+        if(!stats.containsKey(player) || !stats.get(player).containsKey(stat)) return null;
         return stats.get(player).get(stat);
     }
 
-    public int getVar(String var) {
-        return vars.containsKey(var) ? vars.get(var) : 0;
+    public Object getVar(String var) {
+        return vars.containsKey(var) ? vars.get(var) : null;
     }
 
     public List<String> getPlayers() {
@@ -69,7 +67,17 @@ public abstract class MiniGame implements Updateable {
 
     @Override
     public void update() {
-        for (String player : getPlayers()) if (getStat(player, SCORE) == MAX_SCORE) end(player);
+        for (String player : getPlayers()) if (getIntStat(player, SCORE) == MAX_SCORE) end(player);
+    }
+
+    public int getIntStat(String player, String var) {
+        Object val = getStat(player, var);
+        return val == null ? 0 : (Integer) val;
+    }
+
+    public int getIntVar(String var) {
+        Object val = getVar(var);
+        return val == null ? 0 : (Integer) val;
     }
 
     @Override
