b757666b80d69255fb5c5c37e6587748d22cee2e
Aidan Goodwin <Aidan Goodwin>
2017-02-22 15:46:50 +0000

    undoing change since GameLogic fixed

--- src/game/core/world/Location.java
+++ src/game/core/world/Location.java
@@ -40,7 +40,7 @@ public class Location implements Serializable {
     }
 
     public Tile getTile() {
-        return world.getTile(coords.x, coords.y, coords.z);
+        return world.getTile(coords);
     }
 
     public boolean checkBounds() {
