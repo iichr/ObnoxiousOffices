9c8c9114a7433da9bf4683f2689673a7a782ee62
Aidan Goodwin <Aidan Goodwin>
2017-02-27 12:42:52 +0000

    new player avatars

--- src/game/ui/PlayerContainer.java
+++ src/game/ui/PlayerContainer.java
@@ -2,6 +2,7 @@ package game.ui;
 
 import java.util.Set;
 
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -26,10 +27,10 @@ public class PlayerContainer {
 
 	// player names
 	private String[] playerNames;
-	private Image[] playerAvatars;
+	private Animation[] playerAvatars;
 	private double[] playerProgress;
 	private double[] playerFatigue;
-	
+
 	private Image progressBarBase;
 	private Image progressBarFull;
 	private float x;
@@ -37,10 +38,6 @@ public class PlayerContainer {
 
 	private World world;
 	private Set<Player> players;
-
-	// String padding
-	private final int xPad = 10;
-	private final int yPad = 20;
 
 	/**
 	 * A constructor for the player status container
@@ -64,21 +61,34 @@ public class PlayerContainer {
 
 		int size = players.size();
 		playerNames = new String[size];
-		playerAvatars = new Image[size];
+		playerAvatars = new Animation[size];
 		playerProgress = new double[size];
 		playerFatigue = new double[size];
 
 		int i = 1;
 		for (Player p : players) {
+			Image[] temp = new Image[2];
 			if (!p.name.equals(localPlayerName)) {
+				// add name
 				playerNames[i] = p.name;
-				playerAvatars[i] = new Image(ImageLocations.TEMP_AVATAR, false, Image.FILTER_NEAREST);
+
+				// add image
+				temp = setImages(p);
+				playerAvatars[i] = new Animation(temp, 50);
+
+				// add progress and fatigue
 				playerProgress[i] = p.getProgress();
 				playerFatigue[i] = p.status.getAttribute(PlayerAttribute.FATIGUE);
 				i++;
 			} else {
+				// add name
 				playerNames[0] = p.name;
-				playerAvatars[0] = new Image(ImageLocations.TEMP_AVATAR, false, Image.FILTER_NEAREST);
+
+				// add image
+				temp = setImages(p);
+				playerAvatars[0] = new Animation(temp, 50);
+
+				// add progress and fatigue
 				playerProgress[0] = p.getProgress();
 				playerFatigue[0] = p.status.getAttribute(PlayerAttribute.FATIGUE);
 			}
@@ -95,45 +105,75 @@ public class PlayerContainer {
 		final float xSize = Vals.SCREEN_WIDTH / 30;
 		final float ySize = Vals.SCREEN_HEIGHT / 9;
 
-			for (int i = 0; i < playerNames.length; i++) {
-				float xPos = this.x + xSize / 2;
-				float yPos = this.y + ySize * (i + 1);
-				float progress = (float) (playerProgress[i] / 100);
-				float fatigue = (float) (playerFatigue[i] / 1);
-				
-				//player avatar
-				playerAvatars[i].draw(xPos, yPos, xSize, ySize);
-				g.drawString(playerNames[i], xPos + xPad + xSize, yPos);
+		for (int i = 0; i < playerNames.length; i++) {
+			float xPos = this.x + xSize / 3;
+			float yPos = this.y + ySize * (i + 1);
+			float progress = (float) (playerProgress[i] / 100);
+			float fatigue = (float) (playerFatigue[i] / 1);
+			float xPad = xSize/2;
+			float yPad = ySize/6;
 
-				//set values
-				g.setColor(Color.white);
-				float cornerX = xPos + xPad + xSize;
-				float cornerY = yPos + yPad;
-				
-				//progress bar
-				progressBarBase.draw(cornerX, cornerY, xSize * 4, ySize/3);
-				progressBarFull.draw(cornerX, cornerY, cornerX + xSize * 4 * progress, cornerY + ySize/3, 0, 0,
-						progressBarBase.getWidth() * progress, progressBarBase.getHeight());
-				//draw label
-				String progressString = "PROGRESS";
-				float stringWidth = g.getFont().getWidth(progressString);
-				float stringHeight = g.getFont().getHeight(progressString);
-				g.drawString(progressString, cornerX + xSize*2 - stringWidth/2, cornerY + ySize/6 - stringHeight/2);
-				
-				//fatigue bar
-				progressBarBase.draw(cornerX, cornerY + ySize/3, xSize * 4, ySize/3);
-				progressBarFull.draw(cornerX, cornerY + ySize/3, cornerX + xSize * 4 * fatigue, cornerY + 2*ySize/3, 0, 0,
-						progressBarBase.getWidth() * fatigue, progressBarBase.getHeight(), Color.red);
-				//draw label
-				String fatigueString = "FATIGUE";
-				stringWidth = g.getFont().getWidth(fatigueString);
-				stringHeight = g.getFont().getHeight(fatigueString);
-				g.drawString(fatigueString, cornerX + xSize*2 - stringWidth/2, cornerY + ySize/2 - stringHeight/2);
-				
-				//set colour back to black
-				g.setColor(Color.black);
-			}
+			// player avatar
+			playerAvatars[i].draw(xPos, yPos + ySize/6, 2*ySize /3, 2*ySize /3);
+			g.drawString(playerNames[i], xPos + xPad + xSize, yPos);
+
+			// set values
+			g.setColor(Color.white);
+			float cornerX = xPos + xPad + xSize;
+			float cornerY = yPos + yPad;
+
+			// progress bar
+			progressBarBase.draw(cornerX, cornerY, xSize * 4, ySize / 3);
+			progressBarFull.draw(cornerX, cornerY, cornerX + xSize * 4 * progress, cornerY + ySize / 3, 0, 0,
+					progressBarBase.getWidth() * progress, progressBarBase.getHeight());
+			// draw label
+			String progressString = "PROGRESS";
+			float stringWidth = g.getFont().getWidth(progressString);
+			float stringHeight = g.getFont().getHeight(progressString);
+			g.drawString(progressString, cornerX + xSize * 2 - stringWidth / 2, cornerY + ySize / 6 - stringHeight / 2);
+
+			// fatigue bar
+			progressBarBase.draw(cornerX, cornerY + ySize / 3, xSize * 4, ySize / 3);
+			progressBarFull.draw(cornerX, cornerY + ySize / 3, cornerX + xSize * 4 * fatigue, cornerY + 2 * ySize / 3,
+					0, 0, progressBarBase.getWidth() * fatigue, progressBarBase.getHeight(), Color.red);
+			// draw label
+			String fatigueString = "FATIGUE";
+			stringWidth = g.getFont().getWidth(fatigueString);
+			stringHeight = g.getFont().getHeight(fatigueString);
+			g.drawString(fatigueString, cornerX + xSize * 2 - stringWidth / 2, cornerY + ySize / 2 - stringHeight / 2);
+
+			// set colour back to black
+			g.setColor(Color.black);
+		}
 	}
-	
-	
+
+	public void toggleSleep(Player p) {
+		Animation a = playerAvatars[p.getHair()];
+		a.setCurrentFrame((a.getFrame() + 1) % a.getFrameCount());
+	}
+
+	private Image[] setImages(Player p) throws SlickException {
+		Image[] temp = new Image[2];
+		switch (p.getHair()) {
+		case 0:
+			temp[0] = new Image(ImageLocations.BLONDE_HEAD, false, Image.FILTER_NEAREST);
+			temp[1] = new Image(ImageLocations.BLONDE_HEAD_SLEEP, false, Image.FILTER_NEAREST);
+			break;
+		case 1:
+			temp[0] = new Image(ImageLocations.BROWN_HEAD, false, Image.FILTER_NEAREST);
+			temp[1] = new Image(ImageLocations.BROWN_HEAD_SLEEP, false, Image.FILTER_NEAREST);
+			break;
+		case 2:
+			temp[0] = new Image(ImageLocations.DARK_HEAD, false, Image.FILTER_NEAREST);
+			temp[1] = new Image(ImageLocations.DARK_HEAD_SLEEP, false, Image.FILTER_NEAREST);
+			break;
+		case 3:
+			temp[0] = new Image(ImageLocations.PINK_HEAD, false, Image.FILTER_NEAREST);
+			temp[1] = new Image(ImageLocations.PINK_HEAD_SLEEP, false, Image.FILTER_NEAREST);
+			break;
+		}
+		;
+		return temp;
+	}
+
 }
