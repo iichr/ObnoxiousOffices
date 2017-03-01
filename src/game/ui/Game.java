4dda7bdb5e740d8d1e980f19fb3c277bc6a408d6
Aidan Goodwin <Aidan Goodwin>
2017-03-01 20:33:28 +0000

    deleted pause state

--- src/game/ui/Game.java
+++ src/game/ui/Game.java
@@ -2,7 +2,6 @@ package game.ui;
 
 import java.io.File;
 
-import game.core.world.World;
 import org.lwjgl.LWJGLUtil;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
@@ -11,12 +10,12 @@ import org.newdawn.slick.state.StateBasedGame;
 
 import game.core.event.Events;
 import game.core.event.GameStartedEvent;
+import game.core.world.World;
 import game.ui.interfaces.Vals;
 import game.ui.states.CharacterSelect;
 import game.ui.states.Intro;
 import game.ui.states.Menu;
 import game.ui.states.Options;
-import game.ui.states.Pause;
 import game.ui.states.Play;
 import game.ui.states.PlayTest;
 import game.ui.states.Rules;
@@ -28,13 +27,12 @@ public class Game extends StateBasedGame {
 	private Options optionsState;
 	private Rules rulesState;
 	private CharacterSelect chSelectState;
-	private Pause pauseState;
 	private PlayTest playtestState;
 
 	public Game(String gamename) {
 		super(gamename);
-		
-		introState= new Intro(Vals.INTRO_STATE);
+
+		introState = new Intro(Vals.INTRO_STATE);
 		this.addState(introState);
 		menuState = new Menu(Vals.MENU_STATE);
 		this.addState(menuState);
@@ -48,17 +46,14 @@ public class Game extends StateBasedGame {
 		this.addState(rulesState);
 		chSelectState = new CharacterSelect(Vals.CHARACTER_SELECT_STATE, playtestState);
 		this.addState(chSelectState);
-		pauseState = new Pause(Vals.PAUSE_STATE);
-		this.addState(pauseState);
-		
-		
+
 		Events.on(GameStartedEvent.class, this::onGameStart);
 	}
 
 	public void initStatesList(GameContainer gc) throws SlickException {
-		//initialises states automatically
+		// initialises states automatically
 	}
-	
+
 	public void onGameStart(GameStartedEvent event) {
 		World.world = event.world;
 		playState.playSetup();
