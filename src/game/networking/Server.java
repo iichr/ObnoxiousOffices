c3ebd256efcb4b0944f9c46ac3b07fd556e760bc
Aidan Goodwin <Aidan Goodwin>
2017-03-10 10:49:05 +0000

    testing walls

--- src/game/networking/Server.java
+++ src/game/networking/Server.java
@@ -26,7 +26,7 @@ public class Server {
 	private boolean gameEnded = false;
 	private boolean gameStarted = false;
 
-	private final int NUM_PLAYERS = 4;
+	private final int NUM_PLAYERS = 2;
 	
 	public Server() {
 		connections = new ArrayList<ServerListener>();
