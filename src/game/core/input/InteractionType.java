eb71fa56e39308bc0d6761db493a3414f310cdbf
Sam Tebbs <samuel.tebbs@gmail.com>
2017-03-17 15:07:22 +0000

    Added InteractionTypeHack

--- src/game/core/input/InteractionType.java
+++ src/game/core/input/InteractionType.java
@@ -5,6 +5,15 @@ import java.io.Serializable;
 /**
  * Created by samtebbs on 01/03/2017.
  */
-public enum InteractionType implements Serializable {
-    WORK, HACK, SIT, OTHER;
+public class InteractionType implements Serializable {
+    public final InteractionType WORK = new InteractionType(), SIT = new InteractionType(), OTHER = new InteractionType();
+
+    public static class InteractionTypeHack extends InteractionType {
+        public final String target;
+
+        public InteractionTypeHack(String target) {
+            this.target = target;
+        }
+    }
+
 }
