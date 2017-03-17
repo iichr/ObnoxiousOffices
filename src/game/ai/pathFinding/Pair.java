c3c6df725d5ec74a7ca280b24ff21134338d262d
Sam Tebbs <samuel.tebbs@gmail.com>
2017-03-17 16:11:50 +0000

    Fixed ai.Pair.java

--- src/game/ai/pathFinding/Pair.java
+++ src/game/ai/pathFinding/Pair.java
@@ -47,106 +47,6 @@ public class Pair<L, R> implements Serializable{
 	public void setR(R r) {
 		this.r = r;
 	}
-}
-
-import java.io.Serializable;
-
-/**
- * 
- * @author Atanas K. Harbaliev. Created on 14.02.2017
- *
- * @param <L>
- *            left int of pair
- * @param <R>
- *            right int of pair
- */
-public class Pair<L, R> implements Serializable{
-
-	private static final long serialVersionUID = 1L;
-
-	// just for testing
-	@Override
-	public String toString() {
-		return "Pair [l=" + l + ", r=" + r + "]";
-	}
-
-	private L l; // left int
-	private R r; // right int
-
-	// constructor
-	public Pair(L l, R r) {
-		this.l = l;
-		this.r = r;
-	}
-
-	// get methods
-	public L getL() {
-		return l;
-	}
-
-	public R getR() {
-		return r;
-	}
-
-	// set methods
-	public void setL(L l) {
-		this.l = l;
-	}
-
-	public void setR(R r) {
-		this.r = r;
-	}
-}
-
-package game.ai.pathFinding;
-
-import java.io.Serializable;
-
-/**
- * 
- * @author Atanas K. Harbaliev. Created on 14.02.2017
- *
- * @param <L>
- *            left int of pair
- * @param <R>
- *            right int of pair
- */
-public class Pair<L, R> implements Serializable{
-
-	private static final long serialVersionUID = 1L;
-
-	// just for testing
-	@Override
-	public String toString() {
-		return "Pair [l=" + l + ", r=" + r + "]";
-	}
-
-	private L l; // left int
-	private R r; // right int
-
-	// constructor
-	public Pair(L l, R r) {
-		this.l = l;
-		this.r = r;
-	}
-
-	// get methods
-	public L getL() {
-		return l;
-	}
-
-	public R getR() {
-		return r;
-	}
-
-	// set methods
-	public void setL(L l) {
-		this.l = l;
-	}
-
-	public void setR(R r) {
-		this.r = r;
-	}
 	
 	@Override
 	public boolean equals(Object o) {
@@ -168,4 +68,4 @@ public class Pair<L, R> implements Serializable{
 		result = 31 * result + (r != null ? r.hashCode() : 0);
 		return result;
 	}
-}
+}
