541e19e28b17f4fdcbfae051a74f661383672eac
Lucy Mills <lxm569@cca-lg04-13052.cs.bham.ac.uk>
2017-03-23 14:28:12 +0000

    Testing client connection added

--- test/game/networking/NetworkTesting.java
+++ test/game/networking/NetworkTesting.java
@@ -1,16 +1,34 @@
-package game.networking;
+package test.game.networking;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class NetworkTesting {
-	public Server server = new Server();
-	public Client client = new Client();
-	
+import game.core.event.ConnectionAttemptEvent;
+import game.core.event.Events;
+import game.core.sync.ClientSync;
+import game.networking.Client;
+import game.networking.Server;
+
+public class NetworkTesting extends Thread{
+Server server;
+	public void run(){
+		System.out.println("RunningThread");
+		server = new Server();
+	}
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		NetworkTesting thread = new NetworkTesting();
+		thread.start();	
+		ClientSync.init();
+		Client client = new Client();
+		Events.trigger(new ConnectionAttemptEvent("Client1", "Localhost"));
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		assertTrue(client.connected == true);
 	}
 
 }
